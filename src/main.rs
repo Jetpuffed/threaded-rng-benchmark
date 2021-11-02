e31f67442f64578b45342cd0efc38748f9a74a19
Jetpuffed <brandon@jetpuffed.org>
2021-11-02 14:45:01 -0700

    Create a 16 bit unsigned integer generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use core::arch::x86_64::{
+    _rdrand16_step,
+    _rdrand32_step,
+    _rdrand64_step,
+};
+
+fn rand_u16() -> u16
+{
+    let mut n = 0;
+    unsafe { _rdrand16_step(n) };
+
+    return n
 }
+
+fn main() {}
